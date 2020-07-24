244bbc9b9c4d98fc7355d9b037b5fee9684cdbbd
Tsviga Mariia <tsvigam@gmail.com>
2020-07-24 23:50:54 +1000

    Add List of Fishes and print elements.

--- lesson2/Program.cs
+++ lesson2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lesson2
 {
@@ -7,6 +8,27 @@ namespace lesson2
         static void Main(string[] args)
         {
             Writer.Write();
+            var salmons = new List<string>();
+            salmons.Add("chinook");
+            salmons.Add("coho");
+            salmons.Add("pink");
+            salmons.Add("sockeye");
+            salmons.Add("coho");
+            salmons.Add("sockeye");
+            salmons.Add("coho");
+
+            salmons.RemoveAll(coho);
+
+            // Iterate through the list.
+            foreach (var salmon in salmons)
+            {
+                Console.Write(salmon + " ");
+            }
+
+            static bool coho(String s)
+            {
+                return s == "coho" || s == "sockeye";
+            }
         }
     }
 }
